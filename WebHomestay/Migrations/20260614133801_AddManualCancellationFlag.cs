5a610481baf05c89ec6ae221e9b62b1880ac699e
chinhan15102005-cpu <chinhan15102005@gmail.com>
2026-06-14 20:39:48 +0700

    feat: add migration for is_manual column

---
+++ WebHomestay/Migrations/20260614133801_AddManualCancellationFlag.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebHomestay.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddManualCancellationFlag : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "is_manual",
+                table: "booking_cancellation_requests",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "is_manual",
+                table: "booking_cancellation_requests");
+        }
+    }
+}
